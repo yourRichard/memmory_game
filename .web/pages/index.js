f030c642b90c778a6e174e29e3ebf2e895cda85b
Richard Min <Jamesfriend052@gmail.com>
2023-12-13 16:10:15 +0630

    version 1

---
+++ .web/pages/index.js
@@ -0,0 +1,314 @@
+
+
+import { Fragment, useCallback, useContext } from "react"
+import { Fragment_054be72e0ea98018059eac5951b3d7a7 } from "/utils/stateful_components"
+import { Container, HStack, Spacer, Text, VStack } from "@chakra-ui/react"
+import "focus-visible/dist/focus-visible"
+import { EventLoopContext, StateContexts } from "/utils/context"
+import { Event } from "/utils/state"
+import NextHead from "next/head"
+
+
+
+export function Text_9d1f153a287db5c516f31e4583512755 () {
+  const state__state = useContext(StateContexts.state__state)
+  const [addEvents, connectError] = useContext(EventLoopContext);
+
+  const on_click_475eca3b9b104f309f211f76d4cc6645 = useCallback((_e) => addEvents([Event("state.state.reveal_emoji", {emoji:state__state.emoji_list.at(14),emoji_type:`\ud83d\udc83`}),Event("state.state.check_emoji", {})], (_e), {}), [addEvents, Event, state__state])
+
+  return (
+    <Text onClick={on_click_475eca3b9b104f309f211f76d4cc6645} sx={{"fontSize": "32px", "cursor": "pointer", "transition": "opacity 0.55s ease 0.35s", "opacity": state__state.emoji_list.at(14).at(1)}}>
+  {`💃`}
+</Text>
+  )
+}
+
+export function Text_3a86ccacc61ed19af31a8605dab233b6 () {
+  const state__state = useContext(StateContexts.state__state)
+  const [addEvents, connectError] = useContext(EventLoopContext);
+
+  const on_click_58a9c28e0ea08b6029c30d6b02c35d37 = useCallback((_e) => addEvents([Event("state.state.reveal_emoji", {emoji:state__state.emoji_list.at(3),emoji_type:`\ud83c\udf85`}),Event("state.state.check_emoji", {})], (_e), {}), [addEvents, Event, state__state])
+
+  return (
+    <Text onClick={on_click_58a9c28e0ea08b6029c30d6b02c35d37} sx={{"fontSize": "32px", "cursor": "pointer", "transition": "opacity 0.55s ease 0.35s", "opacity": state__state.emoji_list.at(3).at(1)}}>
+  {`🎅`}
+</Text>
+  )
+}
+
+export function Text_738730d95000ae48a578038540e540e1 () {
+  const state__state = useContext(StateContexts.state__state)
+  const [addEvents, connectError] = useContext(EventLoopContext);
+
+  const on_click_1fd13ae038b4f275f7aafaf1ef3b09db = useCallback((_e) => addEvents([Event("state.state.reveal_emoji", {emoji:state__state.emoji_list.at(6),emoji_type:`\ud83e\udd73`}),Event("state.state.check_emoji", {})], (_e), {}), [addEvents, Event, state__state])
+
+  return (
+    <Text onClick={on_click_1fd13ae038b4f275f7aafaf1ef3b09db} sx={{"fontSize": "32px", "cursor": "pointer", "transition": "opacity 0.55s ease 0.35s", "opacity": state__state.emoji_list.at(6).at(1)}}>
+  {`🥳`}
+</Text>
+  )
+}
+
+export function Text_990e256d7fae3fd2cf4444a2c1779ac3 () {
+  const state__state = useContext(StateContexts.state__state)
+  const [addEvents, connectError] = useContext(EventLoopContext);
+
+  const on_click_8ab7b8fe1df453d452a1f6c924c47a10 = useCallback((_e) => addEvents([Event("state.state.reveal_emoji", {emoji:state__state.emoji_list.at(15),emoji_type:`\ud83d\udc83`}),Event("state.state.check_emoji", {})], (_e), {}), [addEvents, Event, state__state])
+
+  return (
+    <Text onClick={on_click_8ab7b8fe1df453d452a1f6c924c47a10} sx={{"fontSize": "32px", "cursor": "pointer", "transition": "opacity 0.55s ease 0.35s", "opacity": state__state.emoji_list.at(15).at(1)}}>
+  {`💃`}
+</Text>
+  )
+}
+
+export function Text_78d39f4f745906cba21f3f1178c88e95 () {
+  const state__state = useContext(StateContexts.state__state)
+  const [addEvents, connectError] = useContext(EventLoopContext);
+
+  const on_click_fec5bddd4f693f459cd28d15a25d247d = useCallback((_e) => addEvents([Event("state.state.reveal_emoji", {emoji:state__state.emoji_list.at(4),emoji_type:`\ud83c\udf81`}),Event("state.state.check_emoji", {})], (_e), {}), [addEvents, Event, state__state])
+
+  return (
+    <Text onClick={on_click_fec5bddd4f693f459cd28d15a25d247d} sx={{"fontSize": "32px", "cursor": "pointer", "transition": "opacity 0.55s ease 0.35s", "opacity": state__state.emoji_list.at(4).at(1)}}>
+  {`🎁`}
+</Text>
+  )
+}
+
+export function Text_be83f238bd148d7c9cac3c7e135b22a8 () {
+  const state__state = useContext(StateContexts.state__state)
+  const [addEvents, connectError] = useContext(EventLoopContext);
+
+  const on_click_a7caf91a0ef5e732d2194559bb46d1e8 = useCallback((_e) => addEvents([Event("state.state.reveal_emoji", {emoji:state__state.emoji_list.at(9),emoji_type:`\ud83d\udc83`}),Event("state.state.check_emoji", {})], (_e), {}), [addEvents, Event, state__state])
+
+  return (
+    <Text onClick={on_click_a7caf91a0ef5e732d2194559bb46d1e8} sx={{"fontSize": "32px", "cursor": "pointer", "transition": "opacity 0.55s ease 0.35s", "opacity": state__state.emoji_list.at(9).at(1)}}>
+  {`💃`}
+</Text>
+  )
+}
+
+export function Text_4edbc36f01f266193657ae553dc8e8d3 () {
+  const state__state = useContext(StateContexts.state__state)
+  const [addEvents, connectError] = useContext(EventLoopContext);
+
+  const on_click_94e92e55a0e4d81eddd36fc6f2392c48 = useCallback((_e) => addEvents([Event("state.state.reveal_emoji", {emoji:state__state.emoji_list.at(8),emoji_type:`\ud83c\udf81`}),Event("state.state.check_emoji", {})], (_e), {}), [addEvents, Event, state__state])
+
+  return (
+    <Text onClick={on_click_94e92e55a0e4d81eddd36fc6f2392c48} sx={{"fontSize": "32px", "cursor": "pointer", "transition": "opacity 0.55s ease 0.35s", "opacity": state__state.emoji_list.at(8).at(1)}}>
+  {`🎁`}
+</Text>
+  )
+}
+
+export function Text_d52dbb4dc123b7d493017acbca215ec0 () {
+  const state__state = useContext(StateContexts.state__state)
+  const [addEvents, connectError] = useContext(EventLoopContext);
+
+  const on_click_0b1cc91a52da7bfdb3f89ff07985ba7c = useCallback((_e) => addEvents([Event("state.state.reveal_emoji", {emoji:state__state.emoji_list.at(11),emoji_type:`\ud83e\udd73`}),Event("state.state.check_emoji", {})], (_e), {}), [addEvents, Event, state__state])
+
+  return (
+    <Text onClick={on_click_0b1cc91a52da7bfdb3f89ff07985ba7c} sx={{"fontSize": "32px", "cursor": "pointer", "transition": "opacity 0.55s ease 0.35s", "opacity": state__state.emoji_list.at(11).at(1)}}>
+  {`🥳`}
+</Text>
+  )
+}
+
+export function Text_0070b52b46734b1a7e1cb43ef8efb5a7 () {
+  const state__state = useContext(StateContexts.state__state)
+  const [addEvents, connectError] = useContext(EventLoopContext);
+
+  const on_click_e473135e6c3cd3ac060c18118b53cd35 = useCallback((_e) => addEvents([Event("state.state.reveal_emoji", {emoji:state__state.emoji_list.at(0),emoji_type:`\ud83e\udd73`}),Event("state.state.check_emoji", {})], (_e), {}), [addEvents, Event, state__state])
+
+  return (
+    <Text onClick={on_click_e473135e6c3cd3ac060c18118b53cd35} sx={{"fontSize": "32px", "cursor": "pointer", "transition": "opacity 0.55s ease 0.35s", "opacity": state__state.emoji_list.at(0).at(1)}}>
+  {`🥳`}
+</Text>
+  )
+}
+
+export function Text_51366695471457a686b7549b22b7c4d9 () {
+  const state__state = useContext(StateContexts.state__state)
+  const [addEvents, connectError] = useContext(EventLoopContext);
+
+  const on_click_3a2fbd3a512217271d4763576ee77fbe = useCallback((_e) => addEvents([Event("state.state.reveal_emoji", {emoji:state__state.emoji_list.at(10),emoji_type:`\ud83c\udf85`}),Event("state.state.check_emoji", {})], (_e), {}), [addEvents, Event, state__state])
+
+  return (
+    <Text onClick={on_click_3a2fbd3a512217271d4763576ee77fbe} sx={{"fontSize": "32px", "cursor": "pointer", "transition": "opacity 0.55s ease 0.35s", "opacity": state__state.emoji_list.at(10).at(1)}}>
+  {`🎅`}
+</Text>
+  )
+}
+
+export function Text_f27bb6f1914b35e608d4f86bbd20da5b () {
+  const state__state = useContext(StateContexts.state__state)
+  const [addEvents, connectError] = useContext(EventLoopContext);
+
+  const on_click_69c87931a57da46d54aa434a80335a35 = useCallback((_e) => addEvents([Event("state.state.reveal_emoji", {emoji:state__state.emoji_list.at(12),emoji_type:`\ud83e\udd73`}),Event("state.state.check_emoji", {})], (_e), {}), [addEvents, Event, state__state])
+
+  return (
+    <Text onClick={on_click_69c87931a57da46d54aa434a80335a35} sx={{"fontSize": "32px", "cursor": "pointer", "transition": "opacity 0.55s ease 0.35s", "opacity": state__state.emoji_list.at(12).at(1)}}>
+  {`🥳`}
+</Text>
+  )
+}
+
+export function Text_4f1a64fa933592f2574ba0d2da8ea464 () {
+  const state__state = useContext(StateContexts.state__state)
+  const [addEvents, connectError] = useContext(EventLoopContext);
+
+  const on_click_62a93fa8adf8f4f341a18aeed9fc6784 = useCallback((_e) => addEvents([Event("state.state.reveal_emoji", {emoji:state__state.emoji_list.at(5),emoji_type:`\ud83d\udc83`}),Event("state.state.check_emoji", {})], (_e), {}), [addEvents, Event, state__state])
+
+  return (
+    <Text onClick={on_click_62a93fa8adf8f4f341a18aeed9fc6784} sx={{"fontSize": "32px", "cursor": "pointer", "transition": "opacity 0.55s ease 0.35s", "opacity": state__state.emoji_list.at(5).at(1)}}>
+  {`💃`}
+</Text>
+  )
+}
+
+export function Text_2f9050fc55ad351ac079829a1209c39c () {
+  const state__state = useContext(StateContexts.state__state)
+  const [addEvents, connectError] = useContext(EventLoopContext);
+
+  const on_click_433501462bcc8080c8ca5ad511c0151d = useCallback((_e) => addEvents([Event("state.state.reveal_emoji", {emoji:state__state.emoji_list.at(2),emoji_type:`\ud83c\udf85`}),Event("state.state.check_emoji", {})], (_e), {}), [addEvents, Event, state__state])
+
+  return (
+    <Text onClick={on_click_433501462bcc8080c8ca5ad511c0151d} sx={{"fontSize": "32px", "cursor": "pointer", "transition": "opacity 0.55s ease 0.35s", "opacity": state__state.emoji_list.at(2).at(1)}}>
+  {`🎅`}
+</Text>
+  )
+}
+
+export function Text_61fee9534ac1697c7b927badec075f46 () {
+  const state__state = useContext(StateContexts.state__state)
+  const [addEvents, connectError] = useContext(EventLoopContext);
+
+  const on_click_721840949e2fd8f134951f007e3f177f = useCallback((_e) => addEvents([Event("state.state.reveal_emoji", {emoji:state__state.emoji_list.at(1),emoji_type:`\ud83c\udf85`}),Event("state.state.check_emoji", {})], (_e), {}), [addEvents, Event, state__state])
+
+  return (
+    <Text onClick={on_click_721840949e2fd8f134951f007e3f177f} sx={{"fontSize": "32px", "cursor": "pointer", "transition": "opacity 0.55s ease 0.35s", "opacity": state__state.emoji_list.at(1).at(1)}}>
+  {`🎅`}
+</Text>
+  )
+}
+
+export function Text_ddc1bb33bce01079ba4d9e4348362a4a () {
+  const state__state = useContext(StateContexts.state__state)
+  const [addEvents, connectError] = useContext(EventLoopContext);
+
+  const on_click_219cde0c9b8849c707d1ccba2e94e0de = useCallback((_e) => addEvents([Event("state.state.reveal_emoji", {emoji:state__state.emoji_list.at(7),emoji_type:`\ud83c\udf81`}),Event("state.state.check_emoji", {})], (_e), {}), [addEvents, Event, state__state])
+
+  return (
+    <Text onClick={on_click_219cde0c9b8849c707d1ccba2e94e0de} sx={{"fontSize": "32px", "cursor": "pointer", "transition": "opacity 0.55s ease 0.35s", "opacity": state__state.emoji_list.at(7).at(1)}}>
+  {`🎁`}
+</Text>
+  )
+}
+
+export function Text_301ba2c829842abc0f4c6013c7d77a17 () {
+  const state__state = useContext(StateContexts.state__state)
+
+
+  return (
+    <Text sx={{"fontSize": "25px", "fontWeight": "bold", "color": "black"}}>
+  {state__state.result}
+</Text>
+  )
+}
+
+export function Text_8d034d411cd8cbb647c8b0f190e8c182 () {
+  const state__state = useContext(StateContexts.state__state)
+  const [addEvents, connectError] = useContext(EventLoopContext);
+
+  const on_click_94fb3b7a00b0c1a736dff4c50b1f25e4 = useCallback((_e) => addEvents([Event("state.state.reveal_emoji", {emoji:state__state.emoji_list.at(13),emoji_type:`\ud83c\udf81`}),Event("state.state.check_emoji", {})], (_e), {}), [addEvents, Event, state__state])
+
+  return (
+    <Text onClick={on_click_94fb3b7a00b0c1a736dff4c50b1f25e4} sx={{"fontSize": "32px", "cursor": "pointer", "transition": "opacity 0.55s ease 0.35s", "opacity": state__state.emoji_list.at(13).at(1)}}>
+  {`🎁`}
+</Text>
+  )
+}
+
+export default function Component() {
+
+  return (
+    <Fragment>
+  <Fragment_054be72e0ea98018059eac5951b3d7a7/>
+  <Container sx={{"bg": "#e0e0e0", "height": "100vh", "maxWidth": "auto", "display": "grid", "position": "relative", "overlay": "hidden", "placeItems": "center"}}>
+  <VStack spacing={`25px`}>
+  <Text sx={{"fontSize": "50px", "fontWeight": "bold", "color": "black"}}>
+  {`Memory game with nextpy`}
+</Text>
+  <Spacer/>
+  <VStack spacing={`15px`}>
+  <HStack spacing={`15px`}>
+  <Container centerContent={true} sx={{"width": "58px", "height": "58px", "background": "#1e293b", "borderRadius": "6px", "cursor": "pointer"}}>
+  <Text_0070b52b46734b1a7e1cb43ef8efb5a7/>
+</Container>
+  <Container centerContent={true} sx={{"width": "58px", "height": "58px", "background": "#1e293b", "borderRadius": "6px", "cursor": "pointer"}}>
+  <Text_61fee9534ac1697c7b927badec075f46/>
+</Container>
+  <Container centerContent={true} sx={{"width": "58px", "height": "58px", "background": "#1e293b", "borderRadius": "6px", "cursor": "pointer"}}>
+  <Text_2f9050fc55ad351ac079829a1209c39c/>
+</Container>
+  <Container centerContent={true} sx={{"width": "58px", "height": "58px", "background": "#1e293b", "borderRadius": "6px", "cursor": "pointer"}}>
+  <Text_3a86ccacc61ed19af31a8605dab233b6/>
+</Container>
+</HStack>
+  <HStack spacing={`15px`}>
+  <Container centerContent={true} sx={{"width": "58px", "height": "58px", "background": "#1e293b", "borderRadius": "6px", "cursor": "pointer"}}>
+  <Text_78d39f4f745906cba21f3f1178c88e95/>
+</Container>
+  <Container centerContent={true} sx={{"width": "58px", "height": "58px", "background": "#1e293b", "borderRadius": "6px", "cursor": "pointer"}}>
+  <Text_4f1a64fa933592f2574ba0d2da8ea464/>
+</Container>
+  <Container centerContent={true} sx={{"width": "58px", "height": "58px", "background": "#1e293b", "borderRadius": "6px", "cursor": "pointer"}}>
+  <Text_738730d95000ae48a578038540e540e1/>
+</Container>
+  <Container centerContent={true} sx={{"width": "58px", "height": "58px", "background": "#1e293b", "borderRadius": "6px", "cursor": "pointer"}}>
+  <Text_ddc1bb33bce01079ba4d9e4348362a4a/>
+</Container>
+</HStack>
+  <HStack spacing={`15px`}>
+  <Container centerContent={true} sx={{"width": "58px", "height": "58px", "background": "#1e293b", "borderRadius": "6px", "cursor": "pointer"}}>
+  <Text_4edbc36f01f266193657ae553dc8e8d3/>
+</Container>
+  <Container centerContent={true} sx={{"width": "58px", "height": "58px", "background": "#1e293b", "borderRadius": "6px", "cursor": "pointer"}}>
+  <Text_be83f238bd148d7c9cac3c7e135b22a8/>
+</Container>
+  <Container centerContent={true} sx={{"width": "58px", "height": "58px", "background": "#1e293b", "borderRadius": "6px", "cursor": "pointer"}}>
+  <Text_51366695471457a686b7549b22b7c4d9/>
+</Container>
+  <Container centerContent={true} sx={{"width": "58px", "height": "58px", "background": "#1e293b", "borderRadius": "6px", "cursor": "pointer"}}>
+  <Text_d52dbb4dc123b7d493017acbca215ec0/>
+</Container>
+</HStack>
+  <HStack spacing={`15px`}>
+  <Container centerContent={true} sx={{"width": "58px", "height": "58px", "background": "#1e293b", "borderRadius": "6px", "cursor": "pointer"}}>
+  <Text_f27bb6f1914b35e608d4f86bbd20da5b/>
+</Container>
+  <Container centerContent={true} sx={{"width": "58px", "height": "58px", "background": "#1e293b", "borderRadius": "6px", "cursor": "pointer"}}>
+  <Text_8d034d411cd8cbb647c8b0f190e8c182/>
+</Container>
+  <Container centerContent={true} sx={{"width": "58px", "height": "58px", "background": "#1e293b", "borderRadius": "6px", "cursor": "pointer"}}>
+  <Text_9d1f153a287db5c516f31e4583512755/>
+</Container>
+  <Container centerContent={true} sx={{"width": "58px", "height": "58px", "background": "#1e293b", "borderRadius": "6px", "cursor": "pointer"}}>
+  <Text_990e256d7fae3fd2cf4444a2c1779ac3/>
+</Container>
+</HStack>
+</VStack>
+  <Spacer/>
+  <Text_301ba2c829842abc0f4c6013c7d77a17/>
+</VStack>
+</Container>
+  <NextHead>
+  <title>
+  {`Nextpy App`}
+</title>
+  <meta content={`A Nextpy app.`} name={`description`}/>
+  <meta content={`favicon.ico`} property={`og:image`}/>
+</NextHead>
+</Fragment>
+  )
+}
---
+++ .web/utils/context.js
@@ -0,0 +1,72 @@
+import { createContext, useContext, useMemo, useReducer, useState } from "react"
+import { applyDelta, Event, hydrateClientStorage, useEventLoop, refs } from "/utils/state.js"
+
+export const initialState = {"state": {"is_hydrated": false, "router": {"session": {"client_token": "", "client_ip": "", "session_id": ""}, "headers": {"host": "", "origin": "", "upgrade": "", "connection": "", "pragma": "", "cache_control": "", "user_agent": "", "sec_websocket_version": "", "sec_websocket_key": "", "sec_websocket_extensions": "", "accept_encoding": "", "accept_language": ""}, "page": {"host": "", "path": "", "raw_path": "", "full_path": "", "full_raw_path": "", "params": {}}}}, "state.state": {"count": 0, "emoji_list": [[0, "0%"], [1, "0%"], [2, "0%"], [3, "0%"], [4, "0%"], [5, "0%"], [6, "0%"], [7, "0%"], [8, "0%"], [9, "0%"], [10, "0%"], [11, "0%"], [12, "0%"], [13, "0%"], [14, "0%"], [15, "0%"], [16, "0%"], [17, "0%"], [18, "0%"], [19, "0%"], [20, "0%"], [21, "0%"], [22, "0%"], [23, "0%"], [24, "0%"], [25, "0%"], [26, "0%"], [27, "0%"], [28, "0%"], [29, "0%"], [30, "0%"], [31, "0%"], [32, "0%"], [33, "0%"], [34, "0%"], [35, "0%"]], "result": "", "score": 0, "track": []}}
+
+export const ColorModeContext = createContext(null);
+export const UploadFilesContext = createContext(null);
+export const DispatchContext = createContext(null);
+export const StateContexts = {
+  state: createContext(null),
+  state__state: createContext(null),
+}
+export const EventLoopContext = createContext(null);
+export const clientStorage = {"cookies": {}, "local_storage": {}}
+
+export const onLoadInternalEvent = () => [Event('state.on_load_internal')]
+
+export const initialEvents = () => [
+    Event('state.hydrate', hydrateClientStorage(clientStorage)),
+    ...onLoadInternalEvent()
+]
+
+export const isDevMode = true
+
+export function UploadFilesProvider({ children }) {
+  const [filesById, setFilesById] = useState({})
+  refs["__clear_selected_files"] = (id) => setFilesById(filesById => {
+    const newFilesById = {...filesById}
+    delete newFilesById[id]
+    return newFilesById
+  })
+  return (
+    <UploadFilesContext.Provider value={[filesById, setFilesById]}>
+      {children}
+    </UploadFilesContext.Provider>
+  )
+}
+
+export function EventLoopProvider({ children }) {
+  const dispatch = useContext(DispatchContext)
+  const [addEvents, connectError] = useEventLoop(
+    dispatch,
+    initialEvents,
+    clientStorage,
+  )
+  return (
+    <EventLoopContext.Provider value={[addEvents, connectError]}>
+      {children}
+    </EventLoopContext.Provider>
+  )
+}
+
+export function StateProvider({ children }) {
+  const [state, dispatch_state] = useReducer(applyDelta, initialState["state"])
+  const [state__state, dispatch_state__state] = useReducer(applyDelta, initialState["state.state"])
+  const dispatchers = useMemo(() => {
+    return {
+      "state": dispatch_state,
+      "state.state": dispatch_state__state,
+    }
+  }, [])
+
+  return (
+    <StateContexts.state.Provider value={ state }>
+    <StateContexts.state__state.Provider value={ state__state }>
+      <DispatchContext.Provider value={dispatchers}>
+        {children}
+      </DispatchContext.Provider>
+    </StateContexts.state__state.Provider>
+    </StateContexts.state.Provider>
+  )
+}
